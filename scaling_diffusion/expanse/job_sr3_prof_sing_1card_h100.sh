7691fd54cf1da3a4fe5aa8cc574789179f4d9862
javierhn <javierhn88@gmail.com>
2026-03-30 12:16:02 -0700

    Added config files, scripts and a README to rerpoduce the results from the Diffusion model scaling

---
+++ scaling_diffusion/expanse/job_sr3_prof_sing_1card_h100.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+#SBATCH --job-name="sr3_1card_h100"
+#SBATCH --output="out_sr3_1card_h100.%j.%N.out"
+#SBATCH --partition=nairr-gpu-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=2
+#SBATCH --gpus=h100:1
+#SBATCH --mem=8G
+#SBATCH --account=sds279
+#SBATCH -t 06:00:00
+
+
+declare -xr SINGUALRITY_MODULE='singularitypro/4.1.2'
+
+module purge
+module load "${SINGUALRITY_MODULE}"
+#module list
+#printenv
+
+WORKDIR=$HOME/models/sr3_scaling_feb26
+
+date
+
+#### We will run from the scratch directory local to the node
+cd /scratch/$USER/job_$SLURM_JOBID
+
+#time -p singularity exec --bind /home,/expanse,/scratch --nv /cm/shared/apps/containers/singularity/pytorch/pytorch_2.2.1-openmpi-4.1.6-mofed-5.8-2.0.3.0-cuda-12.1.1-ubuntu-22.04.4-x86_64-20240412.sif $WORKDIR/run_sr_1gpu.sh #python3 $SLURM_SUBMIT_DIR/main.py
+
+time -p singularity exec --bind /home,/expanse,/scratch --nv /expanse/projects/qstore/installs/containers/singularity/Expanse-Air/pytorch/pytorch-nvcr-25.03.sif $WORKDIR/run_sr_1gpu.sh #python3 $SLURM_SUBMIT_DIR/main.py
+
+#### List scratch directory
+ls /scratch/$USER/job_$SLURM_JOBID
